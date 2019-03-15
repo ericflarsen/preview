2ed5b4ac36c2cd8b095f497e81b95dbd47a3b885
Eric Larsen <larseneric@gmail.com>
2019-03-15 16:21:14 -0500

    Update preview.js

--- preview.js
+++ preview.js
@@ -11,31 +11,32 @@ $(function () {
 
 
 
-});
 
-function bindEvent(element, eventName, eventHandler) {
-    if (element.addEventListener) {
-        element.addEventListener(eventName, eventHandler, false);
-    } else if (element.attachEvent) {
-        element.attachEvent('on' + eventName, eventHandler);
+
+    function bindEvent(element, eventName, eventHandler) {
+        if (element.addEventListener) {
+            element.addEventListener(eventName, eventHandler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent('on' + eventName, eventHandler);
+        }
     }
-}
 
-var iframeEl = document.getElementById('preview-frame'),
-    messageButton = document.getElementById('message_button'),
-    results = document.getElementById('results');
+    var iframeEl = document.getElementById('preview-frame'),
+        messageButton = document.getElementById('message_button'),
+        results = document.getElementById('results');
 
-// Send a message to the child iframe
-var sendMessage = function (msg) {
-    // Make sure you are sending a string, and to stringify JSON
-    iframeEl.contentWindow.postMessage(msg, '*');
-};
-// Send random messge data on every button click
-bindEvent(messageButton, 'click', function (e) {
-    var random = Math.random();
-    sendMessage('' + random);
-});
-// Listen to message from child window
-bindEvent(window, 'message', function (e) {
-    results.innerHTML = e.data;
+    // Send a message to the child iframe
+    var sendMessage = function (msg) {
+        // Make sure you are sending a string, and to stringify JSON
+        iframeEl.contentWindow.postMessage(msg, '*');
+    };
+    // Send random messge data on every button click
+    bindEvent(messageButton, 'click', function (e) {
+        var random = Math.random();
+        sendMessage('' + random);
+    });
+    // Listen to message from child window
+    bindEvent(window, 'message', function (e) {
+        results.innerHTML = e.data;
+    });
 });
